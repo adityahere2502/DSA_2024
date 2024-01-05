86cfb929129758c3999a0d1e6144e9d8e44cb8e4
Aditya Bhakuni <140088779+adityahere2502@users.noreply.github.com>
2024-01-05 22:19:02 +0530

    Add files via upload

--- helpfulmaths.cpp
+++ helpfulmaths.cpp
@@ -1,9 +1,32 @@
 #include <iostream>
-#include <bits/stdc++.h>
+#include <string>
+#include <sstream>
+#include <algorithm>
+
 using namespace std;
-int main(){
-    string s;
-    cin >> s;
-    cout << s << "+" << s;
+
+int main() {
+    string str;
+    cin >> str;
+
+    stringstream ss(str);
+    int numbers[100]; // Assuming a maximum of 100 numbers
+    char plus; // To store the '+' character
+    int count = 0;
+
+    while (ss >> numbers[count]) {
+        ss >> plus; // Read the '+' character
+        ++count;
+    }
+
+    sort(numbers, numbers + count);
+
+    for (int i = 0; i < count; ++i) {
+        cout << numbers[i];
+        if (i != count - 1) {
+            cout << "+";
+        }
+    }
+
     return 0;
 }
