0c7ef90845be4f95e468b2309cb7234e65c32045
Aditya Bhakuni <140088779+adityahere2502@users.noreply.github.com>
2024-01-05 20:11:00 +0530

    Add files via upload

---
+++ watermelon.cpp
@@ -0,0 +1,13 @@
+#include <iostream> 
+using namespace std;
+int main (){
+    int w;
+    // cout << " Enter the input of Watermelon : ";
+    cin >> w;
+    if (w%2 == 0 && w > 2){
+        cout << "YES";
+    }
+    else {
+        cout << " NO";
+    }
+}
